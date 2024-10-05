3b1d6378224c1cf6838d10baec165fd38fc39d0e
Hesam Rad <hesamzakerirad@gmail.com>
2024-10-05 08:55:18 +0330

    Added Google analysis tag.

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -19,7 +19,18 @@
     <link href="/blog/feed.atom" type="application/atom+xml" rel="alternate" title="{{ $page->siteName }} Atom Feed">
 
     @if ($page->production)
-        <!-- Insert analytics code here -->
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-RKMJRVDWWQ"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+
+            gtag('config', 'G-RKMJRVDWWQ');
+        </script>
     @endif
 
     <link rel="preconnect" href="//fdn.fontcdn.ir">
