f16258aa639d8b300eed0519ccb06701a1a41b11
Hesam <hesamzakerirad@gmail.com>
2025-02-13 17:59:53 +0330

    feat: add external link icon

--- source/_components/post-preview-inline.blade.php
+++ source/_components/post-preview-inline.blade.php
@@ -11,11 +11,16 @@
             class="no-decoration"
         >
             {{ $post->title }}
-        </a>
 
-        @if ($post->isExternal)
-            <span class="smaller">(پیوند)</span>
-        @endif
+            @if ($post->isExternal)
+                <span>
+                    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" transform="rotate(-90)">
+                        <path d="M12.5 12L21.5 3V10V3H14.5" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"></path>
+                        <path d="M9.5 3H5.5C4.395 3 3.5 3.895 3.5 5V19C3.5 20.105 4.395 21 5.5 21H19.5C20.605 21 21.5 20.105 21.5 19V15" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"></path>
+                    </svg>
+                </span>
+            @endif
+        </a>
     </p>
 
     <small>
