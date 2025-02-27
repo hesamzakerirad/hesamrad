cdb9c4050cacb5b78c83dca1404dfaa391c1bcf1
Hesam Rad <hesamzakerirad@gmail.com>
2025-02-27 09:57:23 +0330

    Added theme color meta tag to improve UX

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
+    <meta name="theme-color" content="#2455c3" />
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="description" content="{{ $page->description ?? $page->siteDescription }}">
 
