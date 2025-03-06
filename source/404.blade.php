9077ad5fe545b68cea1f0ec7a0256ff883481167
Hesam Rad <hesamzakerirad@gmail.com>
2025-03-06 08:50:30 +0330

    fix: 404 page not being used on production

--- source/404.blade.php
+++ source/404.blade.php
@@ -1,11 +1,13 @@
+---
+title: برگه مورد نظر پیدا نشد.
+permalink: /404.html
+---
+
 @extends('_layouts.main')
 
 @section('body')
-    <div>
-        <h1>404</h1>
-        <h2>صفحه مورد نظر پیدا نشد.</h2>
-        <hr>
-        <p><a href="/">بازگشت به صفحه اول</a>.
-        </p>
+    <div class="wrapper">
+        <h1>برگه مورد نظر پیدا نشد.</h1>
+        <a href="/">دیدن لیست نوشته‌ها</a>
     </div>
 @endsection
