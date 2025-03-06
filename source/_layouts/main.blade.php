e4c0001d5583c1003f64e74011e4df20c4b01a79
Hesam Rad <hesamzakerirad@gmail.com>
2025-03-06 09:22:49 +0330

    fix: remove Google Tag Manager

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -8,27 +8,6 @@
 @endphp
 
 <head>
-    @if ($page->production)
-        <!-- Google Tag Manager -->
-        <script>
-            (function(w, d, s, l, i) {
-                w[l] = w[l] || [];
-                w[l].push({
-                    'gtm.start': new Date().getTime(),
-                    event: 'gtm.js'
-                });
-                var f = d.getElementsByTagName(s)[0],
-                    j = d.createElement(s),
-                    dl = l != 'dataLayer' ? '&l=' + l : '';
-                j.async = true;
-                j.src =
-                    'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-                f.parentNode.insertBefore(j, f);
-            })(window, document, 'script', 'dataLayer', 'GTM-PJ77P9N8');
-        </script>
-        <!-- End Google Tag Manager -->
-    @endif
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
     <meta name="theme-color" content="#2455c3" />
@@ -88,15 +67,6 @@
 </head>
 
 <body>
-    @if ($page->production)
-        <!-- Google Tag Manager (noscript) -->
-        <noscript>
-            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PJ77P9N8" height="0" width="0"
-                style="display:none;visibility:hidden"></iframe>
-        </noscript>
-        <!-- End Google Tag Manager (noscript) -->
-    @endif
-
     <header>
         <section class="identity wrapper">
             <a href="{{ $page->baseUrl }}" class="no-decoration">
