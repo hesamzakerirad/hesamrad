1e5542483526d1f54a6e6496f9b2a258412518ab
Hesam Rad <hesamzakerirad@gmail.com>
2025-03-16 12:16:28 +0330

    feat: redesign homepage to create more whitespace and add breathing room

--- source/_components/post-preview-inline.blade.php
+++ source/_components/post-preview-inline.blade.php
@@ -2,18 +2,22 @@
     $link = $post->source ?? $post->getUrl();
 @endphp
 
-<div class="post-preview">
-    <p>
-        <a 
-            href="{{ $link }}" 
-            title="{{ $post->title }}" 
-            target="{{ $post->isExternal ? '_blank' : '_self' }}"
-        >
-            {{ $post->title }}
-        </a>
-    </p>
-
-    <small>
-        {{ $post->getJalaliDate() }}
-    </small>
-</div>
+<article>
+    <a 
+        href="{{ $link }}" 
+        title="{{ $post->title }}" 
+        target="{{ $post->isExternal ? '_blank' : '_self' }}"
+        class="no-decoration"
+    >
+        <header>
+            <h3>{{ $post->title }}</h3>
+        </header>
+        <p class="description">{{ $post->description }}</p>
+        <small>
+            @if ($post->isExternal)
+                <small class="badge">بازنشر</small> - 
+            @endif
+            <time>{{ $post->getJalaliDate() }}</time>
+        </small>
+    </a>
+</article>
