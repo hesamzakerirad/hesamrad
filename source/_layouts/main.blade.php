483c381ef23f91860b4caac6116824458312a9d2
Hesam Rad <hesamzakerirad@gmail.com>
2025-04-04 22:02:00 +0330

    feat: add Google Analytics

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -9,6 +9,16 @@
 @endphp
 
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-H516TJZR2S"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-H516TJZR2S');
+    </script>
+    
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="copyright" content="{{ $page->siteName }}">
