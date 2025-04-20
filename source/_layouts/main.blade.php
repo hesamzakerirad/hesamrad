c840f9a270c347cc0e11ec4b592d9879ad0a0aca
Hesam Rad <hesamzakerirad@gmail.com>
2025-04-20 18:25:27 +0330

    feat: add new meta/schema tags to improve SEO

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -44,6 +44,8 @@
         <meta property="og:description" content="{{ $description }}">
         <meta property="og:locale" content="{{ $page->locale }}">
         <meta property="og:image" content="{{ $thumbnail }}">
+        <meta property="article:published_time" content="{{ $page->getDate() }}">
+        <meta property="article:modified_time" content="{{ $page->getUpdatedDate() }}">
         <meta property="og:image:width" content="850">
         <meta property="og:image:height" content="470">
 
@@ -52,6 +54,7 @@
         <meta name="twitter:title" content="{{ $title }}">
         <meta name="twitter:description" content="{{ $description }}">
         <meta name="twitter:image" content="{{ $thumbnail }}">
+        <meta name="twitter:image:alt" content="{{ $title }}">
         <meta name="twitter:site" content="@hesamzakerirad">
         <meta name="twitter:creator" content="@hesamzakerirad">
 
@@ -65,9 +68,16 @@
                 "author": {
                   "@type": "Person",
                   "name": "{{ $page->siteName }}"
+                  "url": "{{ $page->baseUrl }}"
                 },
                 "datePublished": "{{ $page->getDate() }}",
-                "image": "{{ $thumbnail }}"
+                "dateModified": "{{ $page->getUpdatedDate() }}",
+                "image": "{{ $thumbnail }}",
+                "timeRequired": "PT{{ $page->getReadTime() }}M",
+                "mainEntityOfPage": {
+                  "@type": "WebPage",
+                  "@id": "{{ $pageUrl }}"
+                }
             }
         </script>
     @endif
@@ -94,9 +104,11 @@
                 <span id="theme" class="theme-toggle">
                     <svg id="theme-icon" width="24" height="24" viewBox="0 0 24 24">
                         <!-- Moon icon (dark color) -->
-                        <path id="moon-icon" fill="currentColor" d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1-8.313-12.454z"/>
+                        <path id="moon-icon" fill="currentColor"
+                            d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1-8.313-12.454z" />
                         <!-- Sun icon (white color in dark mode) -->
-                        <path id="sun-icon" fill="currentColor" d="M12 18a6 6 0 1 1 0-12a6 6 0 0 1 0 12zm0-2a4 4 0 1 0 0-8a4 4 0 0 0 0 8zM11 1h2v3h-2V1zm0 19h2v3h-2v-3zM3.515 4.929l1.414-1.414L7.05 5.636 5.636 7.05 3.515 4.93zM16.95 18.364l1.414-1.414 2.121 2.121-1.414 1.414-2.121-2.121zm2.121-14.85l1.414 1.415-2.121 2.121-1.414-1.414 2.121-2.121zM5.636 16.95l1.414 1.414-2.121 2.121-1.414-1.414 2.121-2.121zM23 11v2h-3v-2h3zM4 11v2H1v-2h3z"/>
+                        <path id="sun-icon" fill="currentColor"
+                            d="M12 18a6 6 0 1 1 0-12a6 6 0 0 1 0 12zm0-2a4 4 0 1 0 0-8a4 4 0 0 0 0 8zM11 1h2v3h-2V1zm0 19h2v3h-2v-3zM3.515 4.929l1.414-1.414L7.05 5.636 5.636 7.05 3.515 4.93zM16.95 18.364l1.414-1.414 2.121 2.121-1.414 1.414-2.121-2.121zm2.121-14.85l1.414 1.415-2.121 2.121-1.414-1.414 2.121-2.121zM5.636 16.95l1.414 1.414-2.121 2.121-1.414-1.414 2.121-2.121zM23 11v2h-3v-2h3zM4 11v2H1v-2h3z" />
                     </svg>
                 </span>
             </div>
