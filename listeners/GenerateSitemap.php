339747054a09d344c4ef1e9a58a9ce1a41f00248
Hesam Rad <hesamzakerirad@gmail.com>
2025-04-25 10:16:31 +0330

    fix: change sitemap generator to add trailing slash to urls to avoid SEO errors

--- listeners/GenerateSitemap.php
+++ listeners/GenerateSitemap.php
@@ -15,26 +15,41 @@ class GenerateSitemap
     ];
 
     public function handle(Jigsaw $jigsaw)
-    {
-        $baseUrl = $jigsaw->getConfig('baseUrl');
+{
+    $baseUrl = $jigsaw->getConfig('baseUrl');
 
-        if (! $baseUrl) {
-            echo("\nTo generate a sitemap.xml file, please specify a 'baseUrl' in config.php.\n\n");
+    if (!$baseUrl) {
+        echo ("\nTo generate a sitemap.xml file, please specify a 'baseUrl' in config.php.\n\n");
+        return;
+    }
 
-            return;
-        }
+    $sitemap = new Sitemap($jigsaw->getDestinationPath() . '/sitemap.xml');
 
-        $sitemap = new Sitemap($jigsaw->getDestinationPath() . '/sitemap.xml');
+    collect($jigsaw->getOutputPaths())
+        ->reject(function ($path) {
+            return $this->isExcluded($path);
+        })
+        ->each(function ($path) use ($baseUrl, $sitemap) {
+            // Homepage
+            if ($path === '') {
+                $url = $baseUrl;
+            } else {
+                $url = $baseUrl . $path;
 
-        collect($jigsaw->getOutputPaths())
-            ->reject(function ($path) {
-                return $this->isExcluded($path);
-            })->each(function ($path) use ($baseUrl, $sitemap) {
-                $sitemap->addItem(rtrim($baseUrl, '/') . $path, time(), Sitemap::DAILY);
+                // Don't add slash if it's a file (has extension like .xml, .txt, .html, etc.)
+                if (! preg_match('/\.\w+$/', $path)) {
+                    // Add trailing slash if missing
+                    if (substr($url, -1) !== '/') {
+                        $url .= '/';
+                    }
+                }
+            }
+
+            $sitemap->addItem($url, time(), Sitemap::DAILY);
         });
 
-        $sitemap->write();
-    }
+    $sitemap->write();
+}
 
     public function isExcluded($path)
     {
