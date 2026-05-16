4e13cf2b9b50e3e24367ea6c18466ac8dc9837c6
Hesam Rad <hesamzakerirad@gmail.com>
2026-05-16 17:02:31 +0330

    chore: did some clean up

--- source/_includes/footer.blade.php
+++ source/_includes/footer.blade.php
@@ -1,3 +1,3 @@
 <footer>
-    <p class="text-center">از اینکه از <a href="{{ $page->baseUrl }}">وب‌سایت</a> من بازدید کردید سپاسگزارم.</p>
+    {{-- <p class="text-center">از اینکه از <a href="{{ $page->baseUrl }}">وب‌سایت</a> من بازدید کردید سپاسگزارم.</p> --}}
 </footer>
