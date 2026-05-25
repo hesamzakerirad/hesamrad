93d45c7cb9886f01ba0c1b582159214419bd735e
Hesam Rad <hesamzakerirad@gmail.com>
2026-05-25 13:34:34 +0330

    fix: update 404 pge in English

--- source/404.blade.php
+++ source/404.blade.php
@@ -7,17 +7,9 @@ robots: noindex,follow
 @extends('_layouts.main')
 
 @section('body')
-    <div class="container">
-        <header class="header">
-            <div class="mb-1">
-                <a href="{{ $page->baseUrl }}" rel="home" aria-label="بازگشت به وبلاگ">
-                    <i class="fa-solid fa-arrow-right ml-05"></i>
-                    بازگشت به وبلاگ
-                </a>
-            </div>
-            <h1>برگه مورد نظر پیدا نشد.</h1>
-            <p>می‌خوای <a href="{{ $page->baseUrl }}">لیست نوشته‌ها</a> رو ببینی؟ شاید چیزی که می‌خواستی رو اونجا پیدا کردی.
-            </p>
-        </header>
+    <div class="container blog mt-3 mb-3">
+        @include('_components.backlink')
+
+        <h1>Not Found.</h1>
     </div>
 @endsection
