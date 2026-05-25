48d26f55779a897881822d3a29e3926f8d6e1894
Hesam Rad <hesamzakerirad@gmail.com>
2026-05-25 14:11:57 +0330

    fix: resolve tiny details

--- source/index.blade.php
+++ source/index.blade.php
@@ -11,7 +11,7 @@
             <h1>I build software.</h1>
 
             <p>I have been building web-based software since 2017 when I was still in college. Every once in a while, I
-                publish something about software enginnering, books or life on my blog; if not I am probabely working on one of my side <a href="{{ $page->baseUrl }}/projects">projects</a>. I also have a literary background so the rest of my time is spent reading books.</p>
+                publish something about software engineering, books or life on my blog; if not, I am probably working on one of my side <a href="{{ $page->baseUrl }}/projects">projects</a>. I also have a literary background so the rest of my time is spent reading books.</p>
 
             <p>I mindfully employ digital minimalism in my life; but you can still reach me by sending an <a
                     href="mailto:hesamrad.dev@gmail.com">email</a> or visiting my <a href="https://linkedin.com/in/hesamrad"
